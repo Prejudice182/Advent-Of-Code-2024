01ece97a61d98dde3ac688c5c749778c55685984
prejudice182 <redpaulkenny@gmail.com>
2025-01-01 20:21:05 +0000

    Finished day 7

--- Day 7/7.js
+++ Day 7/7.js
@@ -1,4 +1,4 @@
-import fs from 'node:fs'
+const fs = require('node:fs')
 
 const input = fs.readFileSync('input.txt', 'utf-8')
 const testData = `190: 10 19
@@ -11,19 +11,33 @@ const testData = `190: 10 19
 21037: 9 7 18 13
 292: 11 6 16 20`
 
-const part1 = (input) => {
-    const permutator = (numbers) => {
-        const operators = ['*', '+']
-        const mixed = numbers.flatMap((value, index, array) => {
-            for 
-        })
-    }
-    const equations = input.split(/\r?\n/).map((equation) => [...equation.matchAll(/\d+/g)].map((match) => match[0]))
-    for (const equation of equations) {
-        const result = equation.shift()
-        console.log(permutator(equation))
-    }
-    //return equations
+const ops = [(a, b) => (a % b === 0 ? a / b : -1), (a, b) => a - b]
+
+const factorOf = (nums, target, i = nums.length - 1) => {
+    if (!i) return target === nums[0]
+    if (target < 0) return false
+    return ops.some((func) => factorOf(nums, func(target, nums[i]), i - 1))
 }
 
-console.log(part1(testData))
+const part1 = (lines) => {
+    const equations = lines.split(/\r?\n/).map((equation) => equation.match(/\d+/g).map(Number))
+    const proper = equations.filter(([target, ...equation]) => factorOf(equation, target)).map(([total]) => total)
+    const total = proper.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+    console.log(total)
+}
+
+const part2 = (lines) => {
+    const unconcat = (x, y) => {
+        const [sub, yMag] = [x - y, 10 ** (Math.floor(Math.log10(y)) + 1)]
+        return sub > 0 && sub % yMag === 0 ? sub / yMag : -1
+    }
+
+    ops.push(unconcat)
+    part1(lines)
+}
+
+part1(testData)
+part1(input)
+
+part2(testData)
+part2(input)
